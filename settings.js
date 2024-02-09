69e787ce41f753f9d2be8517eae6575e94a7d749
walshl444 <157312617+walshl444@users.noreply.github.com>
2024-02-09 12:11:09 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "6803393074:AAFrb551_dkbHJ7ImYfSkjK6_-FBIuRaPbM",
+	chatId: "5935121552",
+};
+
